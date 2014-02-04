55296ed2272347369275edb76a9284bc3bfc248e
Ryan <ryanbyee@gmail.com>
2014-02-04 12:43:27 -0800

    Add passport & cleaned up the server file

--- server/server.js
+++ server/server.js
@@ -1,25 +1,14 @@
 // The App
 var express = require("express");
+var passport = require('passport');
+var path = require('path');
+
+// Create the express server
 var app = express();
-
-// Use static middleware
-app.use(express.static(__dirname + '/public'));
-
-// Middleware with error
-app.use(function(req, res, next){
-  // do something with the request or response
-  next('something went wrong');
-});
-
-// Use the router middleware
-app.use(app.router);
 
 // Get route with multiple middleware
 app.get("/", function (req, res, next) {
-  // Do something with request or response
-  next();
-}, function (req, res) {
-  res.render('view');
+  res.sendfile(path.join(__dirname, '../public/index.html'));
 });
 
 app.use(function(req, res, next){
@@ -31,9 +20,10 @@ var http = require("http");
 var server = http.createServer(app)
 
 // Listen to port 3000 
-console.log("Express server listening on port: 3000");
 server.listen(3000);
+console.log("Express server listening on port: 3000");
 
+// Database connection
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
