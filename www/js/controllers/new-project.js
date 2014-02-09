1ed311efbe0a52479e34af8b2ef603251200f40d
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-02-08 20:44:05 -0800

    Added action sheet to let user choose photo from library or elsewhere

--- www/js/controllers/new-project.js
+++ www/js/controllers/new-project.js
@@ -1,6 +1,6 @@
 angular.module('frankie.controllers')
 
-.controller('NewProjectCtrl', function($scope, $rootScope, $stateParams, $location, ProjectService, currentProject) {
+.controller('NewProjectCtrl', function($scope, $rootScope, $stateParams, $ionicActionSheet, $location, ProjectService, currentProject) {
   
   // Get Data
   // ----------------------------
@@ -47,9 +47,49 @@ angular.module('frankie.controllers')
     $rootScope.$viewHistory.backView.go();
   };
 
-  $scope.takePhoto = function() {
+  // Display action sheet to ask which type of photo to retrieve
+  $scope.selectPhotoType = function() {
+    $ionicActionSheet.show({
+
+      // The various non-destructive button choices
+      buttons: [
+        { text: 'Take Photo' },
+        { text: 'Choose Existing' },
+      ],
+
+      // The text of the cancel button
+      cancelText: 'Cancel',
+
+      // Called when the sheet is cancelled, either from triggering the
+      // cancel button, or tapping the backdrop, or using escape on the keyboard
+      cancel: function() {
+      },
+
+      // Called when one of the non-destructive buttons is clicked, with
+      // the index of the button that was clicked. Return
+      // "true" to tell the action sheet to close. Return false to not close.
+      buttonClicked: function(index) {
+        if (index === 0) {
+          $scope.getPhoto('CAMERA');
+        } else {
+          $scope.getPhoto('PHOTOLIBRARY');
+        }
+        return true;
+      },
+
+      // Called when the destructive button is clicked. Return true to close the
+      // action sheet. False to keep it open
+
+    });
+};
+
+
+  // source type is either 'CAMERA' or 'PHOTOLIBRARY'
+  $scope.getPhoto = function(sourceType) {
+
     navigator.camera.getPicture(onSuccess, onFail, {
       quality: 20,
+      sourceType : Camera.PictureSourceType[sourceType],
       destinationType: Camera.DestinationType.DATA_URL,
       targetWidth: 200,
       targetHeight: 150
